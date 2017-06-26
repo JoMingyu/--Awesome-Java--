6e3f2e1d5b2b6feda5e90261c2f8b61e7c0cfb67
JoMingyu <city7310@naver.com>
2017-06-26 12:17:48 +0900

    Hello World, Variable

---
+++ 001. Hello World/src/com/planb/main/Main.java
@@ -0,0 +1,10 @@
+package com.planb.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		// System class
+		// public static final PrintStream
+	}
+}
---
+++ 002. Variable/src/com/planb/main/Main.java
@@ -0,0 +1,16 @@
+package com.planb.main;
+
+public class Main {
+	public static void main(String[] args) {
+//		long l = 9223372036854775807;
+		int i = 2147483647;
+		short sh = 32767;
+		byte b = 127;
+		
+		float f = (float) 1.5;
+		double d = 1.23;
+		boolean boo = true;
+		
+		char c = 'c';
+	}
+}
