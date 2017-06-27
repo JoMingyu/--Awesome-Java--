a69d213ae481f027b4800bc6d3bc1ff558ec27a2
JoMingyu <city7310@naver.com>
2017-06-27 09:47:55 +0900

    Switch-Case

---
+++ 005. Switch-Case/src/com/planb/main/Main.java
@@ -0,0 +1,40 @@
+package com.planb.main;
+
+public class Main {
+	public static void main(String[] args) {
+		int n = 10;
+		
+		switch(n / 2) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			break;
+		}
+		
+		char c = 'b';
+		
+		switch(c) {
+		case 'a':
+			break;
+		case 'b':
+			break;
+		default:
+			break;
+		}
+		
+		String s = "Hello";
+		
+		switch(s) {
+		case "hello":
+			break;
+		}
+	}
+}
