5a50b04094f8ee131bde4edc76fcbed6c505b94c
JoMingyu <city7310@naver.com>
2017-06-27 09:47:49 +0900

    If

---
+++ 004. If/src/com/planb/main/Main.java
@@ -0,0 +1,15 @@
+package com.planb.main;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		
+		if(a % 3 == 0) {
+			// false로 치환
+		} else if(a % 3 == 1) {
+			// false로 치환
+		} else {
+			// true
+		}
+	}
+}
