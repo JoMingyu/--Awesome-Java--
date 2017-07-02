7f84a8eded9c81cc274ff5b51a4cda24e6c770de
JoMingyu <city7310@naver.com>
2017-07-02 22:38:05 +0900

    POJO

---
+++ 014-2. POJO/src/com/planb/main/Main.java
@@ -0,0 +1,11 @@
+package com.planb.main;
+
+public class Main {
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setName("PlanB");
+		p.setAge(123);
+		
+		System.out.println(p.toString());
+	}
+}
