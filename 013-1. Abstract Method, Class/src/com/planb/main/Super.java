96569b5a5065ed5e0b2545d159d690cfd29ab98b
JoMingyu <city7310@naver.com>
2017-07-02 22:02:34 +0900

    Abstract Method, Class

--- 013-1. Abstract Method, Class/src/com/planb/main/Super.java
+++ 013-1. Abstract Method, Class/src/com/planb/main/Super.java
@@ -1,5 +1,15 @@
 package com.planb.main;
 
 abstract public class Super {
-	abstract void hello();
+	abstract void abstractHello();
+	/*
+	 * body가 없는 메소드
+	 * 클래스에 추상 메소드를 하나 이상 선언할 경우
+	 * 클래스는 추상 클래스가 되어야 함
+	 */
+	
+	void hello() {
+		System.out.println("Hello");
+	}
+	// 추상 클래스엔 일반 메소드도 선언할 수 있음
 }
--- 013-1. Abstract Method, Class/src/com/planb/main/Under.java
+++ 013-1. Abstract Method, Class/src/com/planb/main/Under.java
@@ -2,7 +2,8 @@ package com.planb.main;
 
 public class Under extends Super {
 	@Override
-	void hello() {
+	void abstractHello() {
 		System.out.println("Implemented");
 	}
+	// 상속 시 추상 메소드를 무조건 override해야 함
 }
