be60d28422e19a41c400a6860de0229731a7d412
JoMingyu <city7310@naver.com>
2017-07-03 11:13:13 +0900

    Final Field

---
+++ 025. Final Field/src/com/planb/main/Main.java
@@ -0,0 +1,12 @@
+package com.planb.main;
+
+public class Main {
+	private static final int NUM = 5;
+	// 컨벤션 상 final 변수 이름은 대문자로
+	
+	public static void main(String[] args) {
+		System.out.println(NUM);
+//		NUM = 8;
+		// 재할당 불가
+	}
+}
