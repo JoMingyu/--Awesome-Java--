020070ee8dd8cedfb2625cddd092a87481641f6b
JoMingyu <city7310@naver.com>
2017-07-15 16:17:21 +0900

    Wrapper class

--- 037. Wrapper Class, Boxing&Unboxing/src/com/planb/main/Main.java
+++ 037. Wrapper Class, Boxing&Unboxing/src/com/planb/main/Main.java
@@ -6,12 +6,12 @@ public class Main {
 		// Wrapper class는 특정 기본형 타입을 나타냄
 		
 		a = new Integer(30);
-		// Boxing
+		// Boxing - 기본 타입을 Wrapper class로
 		a = 30;
 		// Auto Boxing
 		
 		int b = a.intValue();
-		// Unboxing
+		// Unboxing - Wrapper class의 인스턴스를 기본 타입으로
 		b = a;
 		// Auto Unboxing
 	}
